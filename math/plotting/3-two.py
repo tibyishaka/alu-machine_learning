7d42dbeb166b591b5aaf05cbb626d7e21993dd35
tibyishaka <t.ibyishaka@alustudent.com>
2026-01-15 15:46:19 +0200

    done with task 3

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -9,10 +9,21 @@ t2 = 1600
 y1 = np.exp((r / t1) * x)
 y2 = np.exp((r / t2) * x)
 
-# your code here
+# Plot the curves
+plt.plot(x, y1, 'r--', label='C-14')   # dashed red line
+plt.plot(x, y2, 'g-', label='Ra-226')  # solid green line
 
-plt.plot(y1,y2)
-plt.title("Exponential Decay of Radiaoactive Elements")
-plt.xlabel("Time(years)")
+# Labels and title
+plt.xlabel("Time (years)")
 plt.ylabel("Fraction Remaining")
+plt.title("Exponential Decay of Radioactive Elements")
+
+# Axis limits
+plt.xlim(0, 20000)
+plt.ylim(0, 1)
+
+# Legend
+plt.legend(loc='upper right')
+
+# Show plot
 plt.show()
